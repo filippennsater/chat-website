7659b747a80c496d7cf2b5d307112023db92759b
filippennsater <filip.pennsater@hotmail.com>
2024-10-27 21:50:47 +0100

    changed the text in IntroSection

--- src/pages/home/IntroSection.jsx
+++ src/pages/home/IntroSection.jsx
@@ -7,8 +7,21 @@ function IntroSection() {
   return (
     <div className='intr-container'>
       <div className='intrc-container'>
+        <div className='intr-text-container'>
+          <div className='intrctext'>
+            Hello and welcome!
+            <br /><br/>
+            I am your virtual HR assistant, designed to quickly and efficiently answer your questions about personnel matters and employment law.<br/>My role is to simplify your workday by providing accurate and up-to-date responses, whether it's about employment conditions, leave requests, or other HR-related topics.
+            <br/>
+            I’m available around the clock to help you find the right information instantly, allowing you to focus on supporting your employees and developing your organization.
+            <br /><br/>
+            Don’t hesitate to ask – I’m here to assist you.
 
+          </div>
+        </div>
+        <div className='intr-image-container'>
 
+        </div>
       </div>
     </div>
   );
