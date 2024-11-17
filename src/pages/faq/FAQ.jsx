905429fe2921fa724de68089ee5765686db1f5d1
filippennsater <filip.pennsater@hotmail.com>
2024-11-17 22:31:16 +0100

    created a FAQ page

---
+++ src/pages/faq/FAQ.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../App.css'
+import FAQSection from './FAQSection';
+
+
+function FAQ (){
+    return (
+    <>
+      <FAQSection/>
+    </>
+    );
+}
+
+export default FAQ;
