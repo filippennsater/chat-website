83067080bf48485a6f2862362f9d7ec99d1a2d5b
filippennsater <filip.pennsater@hotmail.com>
2024-11-18 21:33:02 +0100

    fixing FAQSection

---
+++ src/pages/faq/FAQSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css'
+import './FAQSection.scss';
+
+
+function FAQSection() {
+
+
+    return (
+
+        <div className='FAQ-container'>
+
+        </div>
+    )
+}
+
+export default FAQSection
