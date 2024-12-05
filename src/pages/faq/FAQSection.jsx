e53d62e4755ead1368d8adf09f4c3c07dbd8a1d1
filippennsater <filip.pennsater@hotmail.com>
2024-12-05 23:39:58 +0100

    made dynamic questions and answers

--- src/pages/faq/FAQSection.jsx
+++ src/pages/faq/FAQSection.jsx
@@ -1,44 +1,39 @@
-import React from 'react';
-import '../../App.css'
+import React, { useState, useEffect } from 'react';
+import '../../App.css';
 import './FAQSection.scss';
-
+import faqData from './faqData.json'; // JSON file in the same folder
 
 function FAQSection() {
+  const [faqs, setFaqs] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(null);
 
+  useEffect(() => {
+    // Load FAQs from the JSON file
+    setFaqs(faqData);
+  }, []);
 
-    return (
+  const toggleAnswer = (index) => {
+    // Toggle visibility of the selected answer
+    setActiveIndex(activeIndex === index ? null : index);
+  };
 
-        <div className='FAQ-container'>
-
-            <div className='QS-container'>
-                <div className='FAQ-header-container'>
-                    <div className='FAQ-header'>
-                        FAQS
-                    </div>
-                </div>
-
-                <div className='Q-container'>
-                    <div className='Q-header'>
-                        LOL
-                    </div>
-                </div>
-
-                <div className='Q-container'>
-                    <div className='Q-header'>
-                        LOL
-                    </div>
-                </div>
-
-                <div className='Q-container'>
-                    <div className='Q-header'>
-                        LOL
-                    </div>
-                </div>
-
-            </div>
-
+  return (
+    <div className='FAQ-container'>
+      <div className='QS-container'>
+        <div className='FAQ-header-container'>
+          <div className='FAQ-header'>FAQs</div>
         </div>
-    )
+        {faqs.map((faq, index) => (
+          <div key={index} className='Q-container' onClick={() => toggleAnswer(index)}>
+            <div className='Q-header'>{faq.question}</div>
+            <div className={`Q-answer ${activeIndex === index ? 'show' : ''}`}>
+              {faq.answer}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default FAQSection
+export default FAQSection;
